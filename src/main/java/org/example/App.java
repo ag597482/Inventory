0de7c4552ba01bbd6662f7f97320279eb4826b71
Aman Gupta <aman.gupta@phonepe.com>
2022-05-17 22:44:51 +0530

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class App extends Application<AppConfig> {
+
+    public static void main(final String[] args) throws Exception {
+        new App().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "true";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<AppConfig> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final AppConfig configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
